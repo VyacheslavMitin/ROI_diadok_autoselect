cb7c462e5c477a6a9168574a1aef6b1d803d8962
VyacheslavMitin <vyacheslav.mitin@gmail.com>
2022-04-05 02:17:17 +0400

    работа

---
+++ main.py
@@ -0,0 +1,25 @@
+# Модуль выделения строк в 1С Диадок для отправки
+
+# Импорты
+import time
+import pyautogui as pg
+
+# Переменные
+RANGE = 50  # количество раз прожатия пробела
+TIMEOUT = 0.5  # пауза между прожатиями
+TIMEOUT_SWITCH = 3  # пауза ожидания переключения на 1С
+
+print(f"Необходимо переключиться в 1С в течение {TIMEOUT_SWITCH} секунд")
+time.sleep(TIMEOUT_SWITCH)  # ожидание
+
+pg.press('left', presses=10)  # выбор крайнего левого столбца
+pg.press('space')  # поставить галочку
+time.sleep(TIMEOUT)  # подождать пока 1С поймет что галка появилась
+
+for i in range (RANGE):
+    pg.press('down')  # спуск на стркоу ниже
+    time.sleep(TIMEOUT)
+    pg.press('space')
+    time.sleep(TIMEOUT)
+
+pg.alert("Завершено", "Завершено")  # окно об окончании
